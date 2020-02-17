9fd4a430e64ed32a25563b9aed0d2710f84c896c
Desaruz <37917106+Desaruz@users.noreply.github.com>
2020-02-17 15:29:51 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 const prefix = '!' //prefix
 
+var ping = Date.now() - message.createdTimestamp + " ms";
+
 client.on('ready', () => {
     client.user.setStatus('dnd')
     client.user.setPresence({
@@ -13,11 +15,12 @@ client.on('ready', () => {
     });
 });
 
-client.on('message', message => {
-    if(message.content.startsWith("!ping")) {
-            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
-    }
-}
+if (message.content.startsWith(! + "ping")) {
+    
+    var ping = Date.now() - message.createdTimestamp + " ms";
+    message.channel.sendMessage("Your ping is `" + `${Date.now() - message.createdTimestamp}` + " ms`");
+
+  }
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
