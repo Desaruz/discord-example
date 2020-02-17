4509baa06545684921e3fbef38d567d90851f1e5
Desaruz <37917106+Desaruz@users.noreply.github.com>
2020-02-17 15:24:25 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const prefix = '-' //prefix
+const prefix = '!' //prefix
 
 client.on('ready', () => {
     client.user.setStatus('dnd')
@@ -14,10 +14,10 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('Pong! ' + Math.round(client.ping) + 'ms!');
-  	}
-});
+    if(message.content.startsWith("!ping")) {
+            message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
+    }
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
