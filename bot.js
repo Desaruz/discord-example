6279176c1ff55911ce5f4fa965f55b1c4d7c6cda
Desaruz <37917106+Desaruz@users.noreply.github.com>
2020-02-17 14:38:43 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setStatus('dnd', 'www.gumines.com') 
+    client.user.setStatus('dnd')
+    client.user.setActivity("www.gumines.com");
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
+    	message.reply('Pong');
   	}
 });
 
