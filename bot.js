d1d5a4f51d3701aa1a14ec64a9c700f7c2739822
Desaruz <37917106+Desaruz@users.noreply.github.com>
2020-02-17 14:26:03 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity("www.gumines.com"); 
+    client.user.setStatus('dnd', 'www.gumines.com') 
     console.log('I am ready!');
 });
 
